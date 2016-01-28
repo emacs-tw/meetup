251d8b669d4e8bc5ef6b8a6e1cbb87c73e2c88df
kuanyui <azazabc123@gmail.com>
2016-01-28 20:40:51 +0800

    增加打滴4th的閃電秀簡報

---
+++ 04/閃電秀：Takahashi.js/source.md
@@ -0,0 +1,116 @@
+# 大大安安
+- Hello everyone
+# 自我介紹
+- Self introduce
+# kuanyui
+- (ono hiroko)
+# Emacs 廚
+- +Emacs fanboy+
+# moe-theme.**el**\\moedict.**el**\\fsc.**el**\\ta.**el**\\tldr.**el**\\kaomoji.**el**\\writing-utils.**el**\\fm-bookmarks.**el**\\ox-ioslide.**el** (As contributor)         
+- 9 Emacs plugins
+# 我的 Blog
+- My blog
+# **kuanyui**.github.io
+- Piano, Open Source, Illustration:
+# 進入正題
+- Now let's enter the topic...
+# 高橋流簡報
+- Takahashi Method
+# 發明者
+- Inventor
+# 日本 Ruby 開發者\\**高橋**征義
+- Japanese Ruby developer\\**Takahashi**  Masayoshi
+# 有些人說\\這是偷懶
+- Somebody says "You're too lazy!"
+# 然而我認為高橋流簡報
+- But I think **takahashi method** is
+# 簡單
+- Easy to make
+# 高效
+- Efficient to write
+# 震撼
+- Shock to audiences
+# 偉大的發明
+- Great invention
+# 好東西
+- Good!
+# 寶傑我跟你說...
+- LBJ, do you know...
+# 高橋流簡報...
+- Takahashi Method...
+# 讚!
+- Awesome!
+# 地球只有一個
+- We have only one earth
+# 可是高橋流簡報
+- But with Takahashi Method
+# 卻可以一小時寫出好幾個
+- We can make not only one slide file in one hour
+# 簡報也是有很多種
+- There're various kinds of slide
+# 有的字又小又多\\搞到你完全不想看
+- Some has a lot of small text,\\ which makes you insane
+# 有的根本是在念稿\\棒讀到讓你想睡覺
+- Some people just like oral reading,\\ which makes you sleepy
+# 重點是\\當有各種簡報讓你選擇時
+- （靠北懶得翻譯了...）
+# 高橋，\\我選擇你的簡報
+- +Takahashi, I want you+
+# 高橋流簡報
+- Takahashi Method
+# 讚！
+- Awesome!
+# 所以
+- So
+# takahashi.js
+- +Everybody loves JS+
+# 用 Markdown\\以**最高速率**做簡報
+- Making slides with the fucking highest efficiency \\(in Markdown syntax)
+# （呃，應該說是 Markdown 的子集）
+- (More preciously, a subset of Markdown)
+# 以**純 JS**實現
+- Implement with **pure & native JS**
+# 不用編譯\\即時輸出
+- No compile, export real-time
+# **不**綁任何編輯器
+- No require any editor
+# **不**綁任何平台
+- No require any platform
+# **不**傷身體
+- Harmless to body
+# **不**含類固醇
+- Not contains steroid
+# 只要有瀏覽器就可以用
+- What only need is a (moderm) web browser
+# 讚！
+- Awesome!
+# 可以用\\**全螢幕圖片**
+- **Fullscreen image** is ok
+# ![](https://farm5.staticflickr.com/4070/4318432006_8353d986ab_b.jpg)
+
+# 也可以有**文字**跟**圖片**
+- ...Or image with titles
+![](https://farm7.staticflickr.com/6202/6049737840_e366c03b4f_b.jpg)
+
+# 或者插入 code
+- Code block is also acceptable
+# 用最有震撼力的方式\\展示你的 code
+- Show your code in the most shocking way
+```lisp
+(defun hello-world ()
+  (interactive)
+  (message "高橋流好棒棒"))
+```
+# 也適合拿來酸\\你討厭的程式語言
+- Also suitable for blame a terrible language you hate
+```js
+var shit = this;
+```
+# 讚！
+- Awesome
+# 哪裡取得
+- Where to get
+# GitHub
+# kuanyui/takahashi.js
+# 謝謝大家
+- Thank you
---
+++ 04/閃電秀：Takahashi.js/takahashi.js
@@ -0,0 +1,278 @@
+/* 
+ takahashi.js
+ Created: [2016-01-20 水 19:46]
+ Author: ono hiroko
+ Github: kuanyui/takahashi.js
+ */
+
+onload = function() {
+    function p(x){
+        alert("p() -> " + JSON.stringify(x));
+    }
+    //======================================================
+    // Variables
+    //======================================================
+    
+    var markdownFile = markdownFile || 'source.md';
+    
+    //======================================================
+    // Parser
+    //======================================================
+    
+    function Parser(markdownFileUrl) {
+        this.markdownFileUrl = markdownFileUrl;
+        this.parsed = this.parse(markdownFileUrl);
+    };
+
+    Parser.prototype.getMarkdownFileContentAsString = function () {
+        // XMLHttpRequest
+        var r = new XMLHttpRequest();
+        r.open("GET", this.markdownFileUrl,false); // sync
+        r.overrideMimeType('text/plain; charset=utf-8');
+        r.send();
+        if (r.readyState == 4 && r.status == 200){
+            return r.responseText;
+        } else {
+            return r.status;
+        };
+    };
+
+    Parser.prototype.readLines = function(){
+        var rawString = this.getMarkdownFileContentAsString();
+        return rawString.split("\n");    
+    };
+
+    Parser.prototype.parse = function(){
+        var shit = this;
+        var lines = shit.readLines();
+        shit.parsed = shit.__parse(lines);
+        return shit.parsed;
+    };
+
+    Parser.prototype.__parse = function(lines, parsed){
+        parsed = parsed || [];
+        var imagePattern = /!\[\]\((.+?)\)/;
+        
+        if (lines.length == 0) {
+            return parsed;
+        } else if (lines[0] == ""){
+            return this.__parse(lines.slice(1), parsed);
+        } else if (lines[0].substring(0, 6) == "# ![](") {
+            var imgUrl = lines[0].match(imagePattern)[1];
+            parsed.push({"type": "fullscreen-image", "imgUrl": imgUrl});
+            return this.__parse(lines.slice(1), parsed);
+        } else if (lines[0].substring(0, 4) == "![](") {
+            var imgUrl = lines[0].match(imagePattern)[1];
+            parsed[(parsed.length - 1)]['type'] = "image-and-title";
+            parsed[(parsed.length - 1)]['imgUrl'] = imgUrl;
+            return this.__parse(lines.slice(1), parsed);            
+        } else if (lines[0].substring(0, 2) == "# ") { // Title
+            var title = this.processEmphasisMarks(lines[0].substring(2));
+            parsed.push({"type": "normal", "title": title});
+            return this.__parse(lines.slice(1), parsed);
+        } else if (lines[0].substring(0, 2) == "- ") { // Subtitle
+            var subtitle = this.processEmphasisMarks(lines[0].substring(2));
+            parsed[(parsed.length - 1)]['subtitle'] = subtitle;
+            return this.__parse(lines.slice(1), parsed);
+        } else if (lines[0].substring(0, 3) == "```") {
+            var language = lines[0].substring(3);
+            parsed.push({"type": "codeblock", "language": language, "code": ""});
+            return this.processCodeBlock(lines.slice(1), parsed);
+        } else {
+            return this.__parse(lines.slice(1), parsed);
+        };
+    };
+
+    Parser.prototype.processCodeBlock = function(lines, parsed){
+        if (lines[0].substring(0,3) == "```"){
+            // remove \n at the last line.
+            var code = parsed[(parsed.length - 1)]['code'];
+            parsed[(parsed.length - 1)]['code'] = code.substring(0, code.length-1);
+            return this.__parse(lines.slice(1), parsed);
+        } else {
+            parsed[(parsed.length - 1)]['code'] += lines[0] + "\n";
+            return this.processCodeBlock(lines.slice(1), parsed);
+        };
+    };
+    
+    Parser.prototype.processEmphasisMarks = function(string) {
+        var italic = /\*(.+?)\*/g;
+        var bold   = /\*\*(.+?)\*\*/g;
+        var strike = /\+(.+?)\+/g;
+        var newline = /\\\\/g;
+        var output = string.replace(bold, "<b>$1</b>");
+        output = output.replace(italic, "<i>$1</i>");
+        output = output.replace(strike, "<s>$1</s>");
+        return output.replace(newline, "<br/>");
+    };
+
+    
+    //======================================================
+    // Slide
+    //======================================================
+    // Variable with prefix '$' is a DOM element.
+
+    var availablePageNumbers = [];
+    var currentPageNumber = 0;
+    
+    function generateSlides() {
+        var $slides = document.getElementsByTagName("slides")[0];
+        var parser = new Parser(markdownFile);
+        var slidesData = parser.parsed;
+        for (var i = 0; i < slidesData.length; i++){
+            availablePageNumbers.push(i);
+            var slideData = slidesData[i];
+            var $slide = document.createElement("slide");
+            $slide.id = i;
+            $slide.className = slideData.type;
+            if (slideData.subtitle){
+                $slide.innerHTML += "<h2>" + slideData.subtitle + "</h2>";};
+            if (slideData.title){
+                $slide.innerHTML += "<h1>" + slideData.title + "</h1>";};
+            if (slideData.type=="fullscreen-image"){
+                $slide.innerHTML += "<img class='fullscreen-image' src='" + slideData.imgUrl + "'></img>";};
+            if (slideData.type=="image-and-title"){
+                $slide.innerHTML += "<img class='image-and-title' src='" + slideData.imgUrl + "'></img>";};
+            if (slideData.type=="codeblock"){
+                $slide.innerHTML += "<pre><code class='" + slideData.language + "'>" +
+                    slideData.code + "</code></pre>";};
+            var $h1 = $slide.getElementsByTagName("h1")[0];
+            if ($h1) {fitH1($h1);}
+            var $h2 = $slide.getElementsByTagName("h2")[0];
+            if ($h2) {$h2.style.fontSize = "4em";};
+            $slides.appendChild($slide);
+            fitSlide($slide);
+        }
+    }
+
+    function show(from, to){
+        // FROM page N TO page N
+        document.getElementById(from).style.display = "none";
+        document.getElementById(to).style.display = "block";
+        currentPageNumber = to;
+        location.hash = to;
+    }
+    
+    function getHtmlStringMaxLineLength(HTMLString){
+        var lines = HTMLString.split("<br>");
+        return Math.max.apply({},
+                              lines.map(
+                                  function(line){
+                                      line = line.replace(/<.+?>/g, "");
+                                      line = line.replace(/[\x00-\x7F]{2}/g, "x"); //ascii
+                                      return line.length;}));
+    }
+    
+    document.onkeydown = function(e) {
+        var to = currentPageNumber + {39: 1, 37: -1}[e.which];
+        if (to in availablePageNumbers) {
+            show(currentPageNumber, to);
+        }
+    };
+    
+    function fitSlide($slide){
+        var style = $slide.style;
+        var height = document.body.clientHeight + "px";
+        var width  = document.body.clientWidth + "px";
+        style.height = height;
+        style.width = width;
+        style.display = "none";
+    }
+    
+    function fitH1($ele){
+        var style = $ele.style;
+        var top;
+        var left;
+        var size = ((window.innerWidth / getHtmlStringMaxLineLength($ele.innerHTML)) * 0.76);
+        style.position = "absolute";
+        style.display = "block";
+        style.fontSize = size + "px";
+        style.left = "50%";
+        style.top = "55%";
+        style.transform = "translate(-50%, -50%)";
+        style.margin = "0px";
+        // Shit not work:
+        while ($ele.offsetHeight > window.innerHeight){
+            size = size * 0.95 ;
+            style.fontSize = size + "px";
+        }
+    }
+
+    function resizeAllImages(){
+        var i;
+        /* .fullscreen-image */
+        var fullscreenImages = document.getElementsByClassName("fullscreen-image");
+        if (fullscreenImages.length != 0) {
+            for (i=0; i<fullscreenImages.length; i++) {
+                fullscreenImages[i].style.height = window.innerHeight + "px";
+                fullscreenImages[i].style.width = "auto";
+            }
+        }
+        /* .image-and-title */
+        var images = document.querySelectorAll("img.image-and-title");
+        
+        if (images.length != 0) {
+            for (i=0; i<images.length; i++){
+                var image = images[i];
+                image.style.height = (window.innerHeight * 0.7) + "px";
+                image.style.position = "absolute";
+                image.style.bottom = "30px";
+                image.style.left = "0px"; 
+                image.style.right = "0px"; 
+                image.style.margin = "0 auto";
+                var h1 = image.parentNode.getElementsByTagName("h1")[0];
+                h1.style.top = "150px"; // [FIXME] magic code
+                console.log(image);
+                /* Check if <h2> exist */
+                var h2 = image.parentNode.getElementsByTagName("h2");
+                if (h2.length != 0) {
+                    h1.style.top = "18%"; // [FIXME] magic code
+                    h1.style.fontSize = (window.innerHeight * 0.12) +"px";
+                    h2 = h2[0];
+                    h2.style.position = "absolute";
+                    h2.style.top = "-5%"; // [FIXME] magic code
+                    h2.style.left = 0;
+                    h2.style.right = 0;
+                }
+            }
+        }
+    }
+    
+    //======================================================
+    // Main
+    //======================================================
+
+    function getTextMaxLineLength (text) {
+        var lines = text.split("\n");
+        return Math.max.apply(null,
+                              lines.map(function(line){
+                                  line = line.replace(/[\u4e00-\u9faf\u3000-\u30ff\uff00-\uff60\uffe0-\uffe6]/g, "AA");
+                                  return line.length;
+                              }));
+    }
+    
+    function main(){
+        generateSlides();
+        if (location.hash != "") {
+            currentPageNumber = parseInt(location.hash.substring(1));
+        } else {
+            currentPageNumber = 0;
+        };
+        
+        show(0, currentPageNumber);
+        resizeAllImages();
+        var codeblocks = document.getElementsByTagName("pre");
+        for (var i=0; i<codeblocks.length; i++) {
+            var block = codeblocks[i];
+            var size = ((window.innerWidth / getTextMaxLineLength(block.textContent)) * 1.8);
+            block.style.fontSize = size + "px";
+            block.style.top = "0px";
+            block.style.bottom = "0px";
+            hljs.highlightBlock(block);
+        }
+    }
+
+    main();
+};
+
+
